2a5091299e157a39053b4a78ec6e3bc0cce70605
Halima2014 <benkhalifahalima@hotmail.com>
2022-04-07 12:37:08 +0200

    Create Commande section

---
+++ src/Controller/CommandeController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Entity\Commande;
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+/**
+ * @Route("/admin")
+ */
+class CommandeController extends AbstractController
+{   
+
+    /**
+     * @Route("/voir-les-commandes", name="show_commandes", methods={"GET"})
+     */
+    public function showCommande(CommandeRepository $commandeRepository): Response
+    {
+        return $this->render('admin/show_commandes.html.twig', [
+            'commandes' => $commandeRepository->findBy(['deletedAt' => null])
+        ]);
+    }
+
+    
+    /**
+     * @Route("/archiver-une-commande/{id}", name="soft_delete_commande", methods={"GET"})
+     */
+    public function softDeleteCommande(Commande $commande, EntityManagerInterface $entityManager): Response
+    {
+        $commande->setDeletedAt(new DateTime());
+        $commande->setState('annulée');
+
+        $entityManager->persist($commande);
+        $entityManager->flush();
+
+        $this->addFlash('success', "La commande ".$commande->getId()." a bien été annulée. ");
+        return $this->redirectToRoute('show_commandes');
+    }
+
+    /**
+     * @Route("/voir-une-commande-annulees/", name="show_canceled_commandes", methods={"GET"})
+     */
+    public function showCanceledCommandes(CommandeRepository $commandeRepository): Response
+    {
+
+        $canceledCommandes = $commandeRepository->findByCanceled();
+
+        return $this->render('admin/trash/show_canceled_commandes.html.twig',[
+            'canceled_commandes' =>$canceledCommandes
+        ]);
+    }
+}
